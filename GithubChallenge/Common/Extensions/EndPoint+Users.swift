970a6117779ef641780bb2c185efcda9dfc5252f
Waqas <waqas.naseem.dev@gmail.com>
2025-06-22 01:53:00 +0500

    removes access token

--- GithubChallenge/Common/Extensions/EndPoint+Users.swift
+++ GithubChallenge/Common/Extensions/EndPoint+Users.swift
@@ -14,22 +14,16 @@ extension Endpoint {
     
     static func users() -> Self {
         .init(baseURL: baseUrl,
-              path: "/users",
-              headers: ["Authorization": "Bearer: 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"]
-        )
+              path: "/users")
     }
     
     static func user(with id: Int) -> Self {
         .init(baseURL: baseUrl,
-              path: "/user/\(id)",
-              headers: ["Authorization": "Bearer: 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"]
-        )
+              path: "/user/\(id)")
     }
     
     static func repositories(with username: String) -> Self {
         .init(baseURL: baseUrl,
-              path: "/users/\(username)/repos",
-              headers: ["Authorization": "Bearer: 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"]
-              )
+              path: "/users/\(username)/repos")
     }
 }
